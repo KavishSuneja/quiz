9ab3d8ac56f988ca1b241c063888b42c6ff1d47c
KavishSuneja <sunejakavish15@gmail.com>
2022-11-17 20:12:26 +0530

    savingmine

---
+++ ques.js
@@ -0,0 +1,45 @@
+var quesArray = [ {
+     question:abcd,
+     Option:a,
+     Option:b,
+     Option:c,
+     Option:d,
+    
+    }, 
+
+    {
+     question:abcd,
+     Option:a1,
+     Option:b3,
+     Option:c4,
+     Option:d3,   
+    },
+    
+    {
+     question:dsklfjas,
+     Option:sda,
+     Option:bs,
+     Option:csd,
+     Option:ad,
+    },
+    
+    {
+     question:sadkfl,
+     Option:as,
+     Option:bd,
+     Option:sc,
+     Option:ds,
+    }, 
+    
+    {
+     question:jsldfk,
+     Option:aaa,
+     Option:bbbb,
+     Option:cvvv,
+     Option:vdvzx,
+    }];
+
+    var nextquestionbtn = document.getElementById('sbmitbtn');
+    var questions = document.getElementById('questionS1');
+
+    
