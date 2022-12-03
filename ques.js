b200fb400a01c6d64a9587f9b01965f3c72bee80
vaibhav26432643mittal <vaibhavandanmolmittal2643@gmail.com>
2022-12-03 22:37:09 +0530

    inst

--- ques.js
+++ ques.js
@@ -1,3 +1,82 @@
+//homepage code
+
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor( interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.innerHTML = startValue;
+    if(startValue == endValue){
+      clearInterval(counter);
+    }
+  }, duration ); 
+});
+
+
+
+
+
+
+
+
+window.addEventListener('scroll', reveal);
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+  
+    for(var i = 0; i < reveals.length; i++){
+    
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+    
+      if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+  window.addEventListener('load' , login);
+  effect = document.getElementById('main');
+  function login(){
+   
+   effect.classList.add('blur');
+  }
+  
+ 
+  
+
+
+  // document.getElementById('sub').onclick=function(){check()};
+   function check(){
+     em2 = document.getElementById('email').value;
+  ps2 = document.getElementById('pass').value;
+    if(em2 == "davpakhowal@gmail.com" && ps2=="dav123"){
+      effect.classList.remove('blur');
+      document.getElementById('login').style.display="none";
+    }
+  else{
+    ele55= document.getElementById('com').style.display="block";
+     // alert("Invalid Username/Password")
+     
+     }
+   }
+ 
+
+  //homepage code
+
+
+
+
+
 var quesArray = [ {
      question: "How Respiration Occurs?",
      OptionA:"Plants",
@@ -125,9 +204,10 @@ var quesArray = [ {
 
     var scorePercentage = score/500*100;
     
+function moveon(){
+  document.getElementById('ist-main').style.display="none";
+ document.getElementsByClassName('options').style.zIndex="1000";
+  
+}
 
-
-    
-
-    
 
