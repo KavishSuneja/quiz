bc72ad628bcd347db5d8605c3c96db665a4b5aa9
KavishSuneja <sunejakavish15@gmail.com>
2022-12-04 17:34:10 +0530

    onquesfinishing

--- ques.js
+++ ques.js
@@ -1,4 +1,70 @@
 
+
+ let valueDisplays = document.querySelectorAll(".num");
+ let interval = 1000;
+ 
+ valueDisplays.forEach((valueDisplay) => {
+   let startValue = 0;
+   let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+   let duration = Math.floor( interval / endValue);
+   let counter = setInterval(function () {
+     startValue += 1;
+     valueDisplay.innerHTML = startValue;
+     if(startValue == endValue){
+       clearInterval(counter);
+     }
+   }, duration ); 
+ });
+ 
+ window.addEventListener('scroll', reveal);
+
+  function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+  
+    for(var i = 0; i < reveals.length; i++){
+    
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
+    
+      if(revealtop < windowheight - revealpoint){
+        reveals[i].classList.add('active');
+      }
+      else{
+        reveals[i].classList.remove('active');
+      }
+    }
+  }
+  window.addEventListener('load' , login);
+  effect = document.getElementById('main');
+  function login(){
+   
+   effect.classList.add('blur');
+  }
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var quesArray = [ {
      question: "How Respiration Occurs?",
      OptionA:"Plants",
